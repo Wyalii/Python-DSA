05fbb41eb14e4135f8b3db8b9e7393f0c14b0afd
Wyalii <gigaurigiorgi2727@gmail.com>
2026-06-12 22:50:11 +0400

    made better and complete version of merge sort algorithm.

--- sorting-algos/mergeSort.py
+++ sorting-algos/mergeSort.py
@@ -1,39 +1,73 @@
-def merge_sort(nums: list[int]) -> list[int]:
+# def merge_sort(nums: list[int]) -> list[int]:
     
-    if len(nums) > 1:
-        mid = len(nums)//2
-        leftSide = nums[:mid]
-        rightSide = nums[mid:]
-        merge_sort(leftSide)
-        merge_sort(rightSide)
+#     if len(nums) > 1:
+#         mid = len(nums)//2
+#         leftSide = nums[:mid]
+#         rightSide = nums[mid:]
+#         merge_sort(leftSide)
+#         merge_sort(rightSide)
 
-        i=j=k=0
-        while i < len(leftSide) and j < len(rightSide):
-            if leftSide[i] < rightSide[j]:
-                nums[k] = leftSide[i]
-                i+=1
-            else:
-                nums[k] = rightSide[j]
-                j += 1 
+#         i=j=k=0
+#         while i < len(leftSide) and j < len(rightSide):
+#             if leftSide[i] < rightSide[j]:
+#                 nums[k] = leftSide[i]
+#                 i+=1
+#             else:
+#                 nums[k] = rightSide[j]
+#                 j += 1 
 
-            k+=1    
+#             k+=1    
 
-            while i < len(leftSide):
-             nums[k] = leftSide[i]
-             i += 1
-             k += 1
+#             while i < len(leftSide):
+#              nums[k] = leftSide[i]
+#              i += 1
+#              k += 1
  
-        while j < len(rightSide):
-            nums[k] = rightSide[j]
-            j += 1
-            k += 1
+#         while j < len(rightSide):
+#             nums[k] = rightSide[j]
+#             j += 1
+#             k += 1
         
     
                    
 
-    pass
+#     pass
 
 
-array = [20,15,10]
-merge_sort(array)
-print('sorted array:', array)
+
+
+# array = [20,15,10]
+# merge_sort(array)
+# print('sorted array:', array)
+array = [99,66,11]
+def merge_sort(nums:list[int]) -> list[int]:
+    if len(nums) < 2:
+        return nums
+    
+    mid = len(nums)//2
+    left = merge_sort(nums[:mid])
+    right = merge_sort(nums[mid:])
+    
+    return  merge(left,right)
+    
+def merge(left,right):
+    result = []
+    i = j =0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    while i < len(left):
+        result.append(left[i])
+        i+=1
+
+    while j < len(right):
+        result.append(right[j])
+        j += 1            
+    return result
+
+print(merge_sort(array))
